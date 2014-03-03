833079f97aa76a99485e53e6cc3940f8c0c77b35
Patrick Perini <pcperini@gmail.com>
2014-03-03 13:18:04 -0500

    update bit.ly change mimetype

--- episodes.py
+++ episodes.py
@@ -4,9 +4,9 @@ entries = [
         "description": "Office Hours' pilot episode, wherein Patrick and Ryan sit down with Alec Davis to discuss the death of careers, so-called \"2-sided markets,\" opportunism in the face of a shaky economy, generational identity, the globalization of communities, and the responsibility of opportunists in this new way of operating.\n\nThis month's articles:\n\nMatchmaker, Matchmaker, Find Me a Part-Time Job - NPR (http://www.npr.org/2012/05/31/154009061/matchmaker-matchmaker-find-me-a-part-time-job)\nCareers Are Dead. Welcome to Your Low-Wage, Temp Work Future - Forbes (http://www.forbes.com/sites/jmaureenhenderson/2012/08/30/careers-are-dead-welcome-to-your-low-wage-temp-work-future/)\nFor Ridesharing Apps Like Lyft, Commerce is a Community - WUNC/NPC (http://wunc.org/post/ridesharing-apps-lyft-commerce-community)\nPeer-to-peer rental: The rise of the sharing economy - The Economist (http://www.economist.com/news/leaders/21573104-internet-everything-hire-rise-sharing-economy)",
         "length": 97472596,
         "duration": "48:26",
-        "pubdate": "Mon, 3 Mar 2014 10:00:00 -0500",
-        "filename": "ep0.m4a",
-        "href": "http://bit.ly/1dh9jDo",
+        "pubdate": "Mon, 3 Mar 2014 13:00:00 -0500",
+        "filename": "ep0.mp3",
+        "href": "http://bit.ly/1okADSY",
         "guest":
         {
             "name": "Alec Davis",
