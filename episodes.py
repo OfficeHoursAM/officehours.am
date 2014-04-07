577bd4ba759387e8477530cbeaedd4fea5269bbd
Patrick Perini <pcperini@gmail.com>
2014-04-07 10:17:42 -0400

    add ep 1

--- episodes.py
+++ episodes.py
@@ -2,7 +2,6 @@ entries = [
     {
         "title": "The Microtransaction Generation",
         "description": "Office Hours' pilot episode, wherein Patrick and Ryan sit down with Alec Davis to discuss the death of careers, so-called \"2-sided markets,\" opportunism in the face of a shaky economy, generational identity, the globalization of communities, and the responsibility of opportunists in this new way of operating.\n\nThis month's articles:\n\nMatchmaker, Matchmaker, Find Me a Part-Time Job - NPR (http://www.npr.org/2012/05/31/154009061/matchmaker-matchmaker-find-me-a-part-time-job)\nCareers Are Dead. Welcome to Your Low-Wage, Temp Work Future - Forbes (http://www.forbes.com/sites/jmaureenhenderson/2012/08/30/careers-are-dead-welcome-to-your-low-wage-temp-work-future/)\nFor Ridesharing Apps Like Lyft, Commerce is a Community - WUNC/NPC (http://wunc.org/post/ridesharing-apps-lyft-commerce-community)\nPeer-to-peer rental: The rise of the sharing economy - The Economist (http://www.economist.com/news/leaders/21573104-internet-everything-hire-rise-sharing-economy)",
-        "length": 97472596,
         "duration": "48:26",
         "pubdate": "Mon, 3 Mar 2014 13:00:00 -0500",
         "filename": "ep0.mp3",
@@ -11,6 +10,19 @@ entries = [
         {
             "name": "Alec Davis",
             "href": "http://twitter.com/alecat1008"
+        }
+    },
+    {
+        "title": "Measuring the Value of Entrepreneurship",
+        "description": "Office Hours' first official episode, wherein Patrick and Ryan chat with James Lomuscio about how to define entrepreneurship, small businesses in New York vs. Pittsburgh, \"work vs. labor,\" innovation, the nature of ownership, and Pope Leo XIII's \"Rerum Novarum\".\n\nThis month's articles:\n\nRegional Insights: Pittsburgh region is one of the least entrepreneurial places - Post Gazette (http://bit.ly/1jmVg0H)\nDefining and Measuring Entrepreneurship for Region Research: A New Apporach - Dissertation, Sarah A. Low (http://bit.ly/OphvYu) Chapters 2 and 3",
+        "duration": "53:26",
+        "pubdate": "Mon, 7 Apr 2014 10:00:00 -0500",
+        "filename": "ep1.mp3",
+        "href": "http://bit.ly/1gDbQot",
+        "guest":
+        {
+            "name": "James Lomuscio",
+            "href": "http://twitter.com/lomuscio"
         }
     }
 ]
